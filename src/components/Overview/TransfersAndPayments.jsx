e473f64a4ee89c534b3811e65ea89832405b947b
Petronik <petronik@ukr.net>
2022-02-18 00:23:37 +0200

    added Links in component

--- src/components/Overview/TransfersAndPayments.jsx
+++ src/components/Overview/TransfersAndPayments.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import TopUp from '../../assets/img/TopUp.svg'
 import Money from '../../assets/img/SendMoney.svg'
 import Withdraw from '../../assets/img/Withdraw.svg'
@@ -10,18 +10,18 @@ const TransfersAndPayments = () => {
     <section className={styles.transfersContainer} >
       <h3>Transfers and Payments</h3>
       <div className={styles.transfersButtonBlock}>
-        <div className={styles.transfersButton}>
+        <Link to={'/transfers/topup'} className={styles.transfersButton}>
           <img src={TopUp} alt="TopUp icon" />
           <span>Top up balance</span>
-        </div>
-        <div className={styles.transfersButton}>
+        </Link>
+        <Link to={'/transfers/sendmoney'} className={styles.transfersButton}>
           <img src={Money} alt="Money icon" />
           <span>Send money</span>
-        </div>
-        <div className={styles.transfersButton}>
+        </Link>
+        <Link to={'/transfers/withdraw'} className={styles.transfersButton}>
           <img src={Withdraw} alt="Withdraw icon" />
           <span>Other payments</span>
-        </div>
+        </Link>
       </div>
     </section>
   );
